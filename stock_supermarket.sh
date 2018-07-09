1cbaf24a587165263e6e6f641be6ca399a427ae9
niugrace <grace.niu.01@gmail.com>
2018-07-09 19:31:43 +0000

    Done.

---
+++ stock_supermarket.sh
@@ -0,0 +1,34 @@
+mv kwk-students-l1-det-070918/delivery/apples.txt fruit
+mv kwk-students-l1-det-070918/delivery/bison_burgers.txt meat
+mv kwk-students-l1-det-070918/delivery/brie.txt
+mv kwk-students-l1-det-070918/delivery/broccoli.txt
+mv kwk-students-l1-det-070918/delivery/carrot_cake.txt
+mv kwk-students-l1-det-070918/delivery/eggo_waffles.txt
+mv kwk-students-l1-det-070918/delivery/fage_yogurt.txt
+mv kwk-students-l1-det-070918/delivery/fig_bars/txt
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/delivery/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
