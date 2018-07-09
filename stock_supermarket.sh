936d044d22db97251509e4ef0e26e0b5d274c059
niugrace <grace.niu.01@gmail.com>
2018-07-09 19:01:27 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -1,3 +1,34 @@
 mv kwk-students-l1-det-070918/delivery/apples.txt fruit
 mv kwk-students-l1-det-070918/delivery/bison_burgers.txt meat
-mv kwk-students-l1-det-070918
+mv kwk-students-l1-det-070918/delivery/brie.txt
+mv 
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
+mv
