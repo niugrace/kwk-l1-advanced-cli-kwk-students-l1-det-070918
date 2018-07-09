006f5d9290d4298776e7107f5d68aa8c0dad8386
niugrace <grace.niu.01@gmail.com>
2018-07-09 19:01:54 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -1,9 +1,9 @@
 mv kwk-students-l1-det-070918/delivery/apples.txt fruit
 mv kwk-students-l1-det-070918/delivery/bison_burgers.txt meat
 mv kwk-students-l1-det-070918/delivery/brie.txt
+mv kwk-students-l1-det-070918/
+mv kwk-students-l1-det-070918/
 mv 
-mv
-mv
 mv
 mv
 mv
